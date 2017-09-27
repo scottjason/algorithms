b2062d4dbd8e35b5bd298380d01969cf8865686e
Ben Ede <ben.ede@gmail.com>
2017-09-27 08:19:56 -0700

    fixed after rename :P

--- ben-v1.rb
+++ ben-v1.rb
@@ -1,15 +1,12 @@
 search = 'be'
 target = 'betterbeabeer soup bea'
 
-target.split(s)
-
-res = target.reverse.split(s.reverse)
+res = target.reverse.split(search.reverse)
 
 slen = search.length
 arr=[]
 res.each do |e|
   idx = target.rindex(search+e.reverse) + slen
   arr << [idx, target.length-1]
-  target = str.chomp(search+e.reverse)
+  target = target.chomp(search+e.reverse)
 end
-arr
